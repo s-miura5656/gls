dfd5c371a295342e01c18d0bad52335718a9093e
souta_miura <miura042910@gmail.com>
2019-09-19 16:46:54 +0900

    ステージにギミックを追加

---
+++ Assets/miura/Script/Bullet_Move.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_Move : MonoBehaviour
+{
+    // 弾が出た瞬間の位置
+    private Vector3 base_pos;
+    // 移動距離
+    private float moving_distance;
+    // １フレーム当たりの移動量
+    private float move_bullet_z;
+    private float move_bullet_x;
+    // 移動量
+    private float dis;
+    // Start is called before the first frame update
+    void Start()
+    {
+        base_pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dis = Vector3.Distance(base_pos, transform.position);
+
+        // 弾の移動処理
+        transform.position += new Vector3(move_bullet_x, 0f, move_bullet_z);
+        DestroyBullet();
+    }
+
+    /// <summary>
+    /// 一定距離進んだら消える
+    /// </summary>
+    private void DestroyBullet()
+    {
+        if (dis >= moving_distance)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    /// <summary>
+    /// 進む距離を入れる
+    /// </summary>
+    public void MovingDistance(float number, float move_z, float move_x)
+    {
+        moving_distance = number;
+        move_bullet_z = move_z;
+        move_bullet_x = move_x;
+    }
+}
--- Assets/miura/Script/Shot_Block.cs
+++ Assets/miura/Script/Shot_Block.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class Shot_Block : MonoBehaviour
 {
+    // 時間のカウント
+    private float time;
+    // 撃つ間隔の変数
+    [SerializeField]
+    private float shot_time;
+    // 弾のオブジェクト
+    private GameObject bullet;
+    // 弾のコピー
+    private GameObject bullet_copy;
+    // コピーした弾のスクリプトを取得
+    private Bullet_Move bulletmove_script;
+    // 弾の進む距離
+    [SerializeField]
+    private float move_distance;
+    // １フレーム当たりの移動量
+    [SerializeField]
+    private float move_bullet_z;
+    [SerializeField]
+    private float move_bullet_x;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        bullet = (GameObject)Resources.Load("Bullet");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ShotInterval();  
+    }
+
+    /// <summary>
+    /// 一定間隔で弾を撃つ
+    /// </summary>
+    private void ShotInterval()
+    {
+        time += Time.deltaTime;
+
+        if (time >= shot_time)
+        {
+            bullet_copy = Instantiate(bullet, transform.position, transform.rotation);
+            bulletmove_script = bullet_copy.GetComponent<Bullet_Move>();
+            bulletmove_script.MovingDistance(move_distance, move_bullet_z, move_bullet_x);
+            time = 0;
+        }
     }
 }
