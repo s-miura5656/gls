de9829bf3ca5ed3a7ad524103fd813f2feef5d01
souta_miura <miura042910@gmail.com>
2019-11-08 16:50:58 +0900

    街の調整

--- Assets/miura/Script/Plus_One_Sec_Destroy.cs
+++ Assets/miura/Script/Plus_One_Sec_Destroy.cs
@@ -7,25 +7,21 @@ public class Plus_One_Sec_Destroy : MonoBehaviour
     private GameObject player;
     private float obj_pos_y = 0f;
     private float base_scale_y;
-    private GameObject main_camera;
+
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player1");
-        main_camera = GameObject.Find("MainCamera");
         base_scale_y = player.transform.localScale.y;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.LookAt(main_camera.transform);
-
-        //transform.localScale = new Vector3(0.01f, 0.01f, 0.01f) * (player.transform.localScale.y);
         transform.position = new Vector3(player.transform.position.x, player.transform.localScale.y + obj_pos_y, player.transform.position.z);
         obj_pos_y += 0.1f;
 
-        if (transform.position.y >= player.transform.localScale.y + 6f)
+        if (transform.position.y >= player.transform.localScale.y + 10f)
         {
             Destroy(gameObject);
         }
