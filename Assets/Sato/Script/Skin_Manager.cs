84d87d413c440eea5154237aff07534d0b29c3b0
yspororosan <ys_pororo@yahoo.jp>
2019-11-09 15:56:12 +0900

    スキン画面の作成中

--- Assets/Sato/Script/Skin_Manager.cs
+++ Assets/Sato/Script/Skin_Manager.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 
 public class Skin_Manager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject title;
+    [SerializeField]
+    private GameObject skin;
+    [SerializeField]
+    private float _animtime = 1.0f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +29,23 @@ public class Skin_Manager : MonoBehaviour
 
     public void PlayGame()
     {
-        SceneManager.LoadScene("Title_");
+        //RectTransform rectTransform = skin.GetComponent<RectTransform>();
+
+        
+        var recttransform_1 = skin.GetComponent<RectTransform>();
+        recttransform_1.DOScaleY(
+              0.0f,　　//終了時点のScale
+    _animtime 　　　　　　//時間
+            );
+
+
+
+        //recttransform_1.offsetMin -= new Vector2(0, -10);
+        //recttransform_1.offsetMax += new Vector2(0, -100);
+        //skin.gameObject.offsetMin = new Vector2(left, bottom);
+
+        //skin.GetComponent<RectTransform>().top = new Vector2(left, bottom);
+        //GetComponent<RectTransform>().offsetMax = new Vector2(right, top);
+        //SceneManager.LoadScene("Title_");
     }
-}
+    }
