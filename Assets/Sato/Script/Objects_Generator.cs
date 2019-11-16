766435e6af243e9d10fbd5e33f18dd246672e503
souta_miura <miura042910@gmail.com>
2019-11-17 01:37:08 +0900

    ゲームシーンのオブジェクトのプレハブ化

---
+++ Assets/Sato/Script/Objects_Generator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objects_Generator : MonoBehaviour
+{
+    [SerializeField] private GameObject[] game_objects;
+
+    private void Awake()
+    {
+        for (int i = 0; i < game_objects.Length; i++)
+        {
+            Instantiate(game_objects[i]);
+        }
+    }
+}
