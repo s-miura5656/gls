1821c58eba1673de9affddf76e59ef85cdd98675
yspororosan <ys_pororo@yahoo.jp>
2019-11-22 15:05:09 +0900

    広告でない

---
+++ Assets/Sato/Script/Botton_clik.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Botton_clik : MonoBehaviour
+{
+
+    private int skin_number;
+    
+    private void Number0_Clik()
+    {
+
+        skin_number = 0;
+    }
+
+    private void Number1_Clik()
+    {
+
+        skin_number = 1;
+    }
+
+    private void Number2_Clik()
+    {
+
+        skin_number = 2;
+    }
+
+    private void Number3_Clik()
+    {
+
+        skin_number = 3;
+    }
+
+    private void Number4_Clik()
+    {
+
+        skin_number = 4;
+    }
+
+    private void Number5_Clik()
+    {
+
+        skin_number = 5;
+
+    }
+
+
+
+}
