89edca0343da1bbc1944b6d48a2d726bd800d8c3
yspororosan <ys_pororo@yahoo.jp>
2019-11-24 11:39:51 +0900

    リザルト　破壊率導入中

--- Assets/Sato/Script/CrashScore_Manager.cs
+++ Assets/Sato/Script/CrashScore_Manager.cs
@@ -22,8 +22,10 @@ public class CrashScore_Manager : MonoBehaviour
     private Text crash_score_text;
 
 
-    private float crash_count;
+    public float crash_count;
     private int test_count;
+
+    private int crash_rate;
 
 
 
@@ -31,6 +33,7 @@ public class CrashScore_Manager : MonoBehaviour
     {
         crash_count = Variable_Manager.Instance.GetSetDestructionRate;
         Crash_Manager();
+        Crash_Bonusew();
 
     }
 
@@ -52,6 +55,67 @@ public class CrashScore_Manager : MonoBehaviour
             5.0f                  // アニメーション時間
         ).SetEase(Ease.OutCubic);
     }
+
+
+    public void Crash_Bonusew()
+    {
+        //if(crash_count >= 0 && crash_count > 10)
+        //{
+        //    crash_rate = 0;
+        //}
+
+        //else if(crash_count >= 10 && crash_count > 20)
+        //{
+        //    crash_rate = 10;
+        //}
+
+        //else if (crash_count >= 20 && crash_count > 30)
+        //{
+        //    crash_rate = 20;
+        //}
+
+        //else if (crash_count >= 30 && crash_count > 40)
+        //{
+        //    crash_rate = 30;
+        //}
+
+        //else if (crash_count >= 40 && crash_count > 50)
+        //{
+        //    crash_rate = 40;
+        //}
+
+        //else if (crash_count >= 50 && crash_count > 60)
+        //{
+        //    crash_rate = 50;
+        //}
+
+        //else if (crash_count >= 60 && crash_count > 70)
+        //{
+        //    crash_rate = 60;
+        //}
+
+        //else if (crash_count >= 70 && crash_count > 80)
+        //{
+        //    crash_rate = 70;
+        //}
+
+        //else if (crash_count >= 80 && crash_count > 90)
+        //{
+        //    crash_rate = 80;
+        //}
+
+        //else if (crash_count >= 90 && crash_count <= 100)
+        //{
+        //    crash_rate = 90;
+        //}
+
+
+
+
+
+    }
+
+
 }
 
-//Crash_Manager() の最終的な値に　破壊率を入れてください
+
