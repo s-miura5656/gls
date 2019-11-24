69a38a7b1557b0d9a7785a048fd4a6bd756b00cc
souta_miura <miura042910@gmail.com>
2019-11-24 14:13:37 +0900

    ライト調整中

--- Assets/Sato/Script/Script/Player_Exp_Get.cs
+++ Assets/Sato/Script/Script/Player_Exp_Get.cs
@@ -9,6 +9,10 @@ public class Player_Exp_Get : MonoBehaviour
 {
     // 取得経験値
     private int exp = 0;
+    // 破壊した時に手に入るコイン
+    private int[] coin = new int [6];
+    // 今回のゲームで取得したコイン
+    private int get_coin = 0;
     // 倒したときに手に入る経験値（レベル１）
     private int[] get_exp = new int[6];
     // プレイヤーのレベルを管理しているスクリプトを取得
@@ -30,12 +34,20 @@ public class Player_Exp_Get : MonoBehaviour
         get_exp[3] = 200;
         get_exp[4] = 500;
         get_exp[5] = 1000;
+
+        coin[0] = 1;
+        coin[1] = 3;
+        coin[2] = 5;
+        coin[3] = 10;
+        coin[4] = 30;
+        coin[5] = 50;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        now_coin.text = "Coin:" + exp;
+        now_coin.text = "Coin:" + get_coin;
         SetCoin();
     }
 
@@ -46,11 +58,12 @@ public class Player_Exp_Get : MonoBehaviour
     public void SetExp(int bill_level) 
     {
         exp += get_exp[bill_level];
+        get_coin += coin[bill_level];
     }
 
     public void SetCoin()
     {
-        Variable_Manager.Instance.GetSetCoin = exp;
+        Variable_Manager.Instance.GetSetCoin = get_coin;
     }
 
     private void Reset()
