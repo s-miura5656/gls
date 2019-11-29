5f688412de1f3a20e4e249250186ecd232a2ffe7
yspororosan <ys_pororo@yahoo.jp>
2019-11-29 12:59:30 +0900

    スキン画面新仕様に変更中

--- Assets/Sato/Script/PossessionCoin_Manager.cs
+++ Assets/Sato/Script/PossessionCoin_Manager.cs
@@ -14,14 +14,127 @@ public class PossessionCoin_Manager : MonoBehaviour
 
     private Variable_Manager script;
 
+
+
+    [SerializeField]
+    private GameObject botton_2;
+    [SerializeField]
+    private Text botton_2_text;
+    [SerializeField]
+    private GameObject botton_2_image;
+
+    [SerializeField]
+    private GameObject botton_3;
+    [SerializeField]
+    private Text botton_3_text;
+    [SerializeField]
+    private GameObject botton_3_image;
+
+    [SerializeField]
+    private GameObject botton_4;
+    [SerializeField]
+    private Text botton_4_text;
+    [SerializeField]
+    private GameObject botton_4_image;
+
+    [SerializeField]
+    private GameObject botton_5;
+    [SerializeField]
+    private Text botton_5_text;
+    [SerializeField]
+    private GameObject botton_5_image;
+
+    [SerializeField]
+    private GameObject botton_6;
+    [SerializeField]
+    private Text botton_6_text;
+    [SerializeField]
+    private GameObject botton_6_image;
+
+    private int skin_after_coin;
+
+
+
+
+
     void Start()
     {
         possession_coin = Variable_Manager.Instance.GetSetPossessionCoin;
+        possession_coin = 10000;
         get_coin_text.text = " " + possession_coin;
     }
 
     private void Awake()
     {
         Application.targetFrameRate = 30;
+    }
+
+
+    public void Skin_open1000_2()
+    {
+
+        
+        if(possession_coin >= 1000)
+        {
+            skin_after_coin = possession_coin - 1000;
+            botton_2_image.SetActive(false);
+            botton_2_text.text = "-";
+            get_coin_text.text = " " + skin_after_coin;
+        }
+
+    }
+
+    public void Skin_open1000_3()
+    {
+
+
+        if (possession_coin >= 1000)
+        {
+            skin_after_coin = possession_coin - 1000;
+            botton_3_image.SetActive(false);
+            botton_3_text.text = "-";
+            get_coin_text.text = " " + skin_after_coin;
+        }
+
+    }
+
+    public void Skin_open1000_4()
+    {
+
+
+        if (possession_coin >= 1000)
+        {
+            skin_after_coin = possession_coin - 1000;
+            botton_4_image.SetActive(false);
+            botton_4_text.text = "-";
+            get_coin_text.text = " " + skin_after_coin;
+        }
+
+    }
+
+    public void Skin_open4000()
+    {
+        if (possession_coin >= 4000)
+        {
+            skin_after_coin = possession_coin - 4000;
+            botton_5_image.SetActive(false);
+            botton_5_text.text = "-";
+            get_coin_text.text = " " + skin_after_coin;
+        }
+
+    }
+
+    public void Skin_open5000()
+    {
+
+        
+        if (possession_coin >= 5000)
+        {
+            skin_after_coin = possession_coin - 5000;
+            botton_6_image.SetActive(false);
+            botton_6_text.text = "-";
+            get_coin_text.text = " " + skin_after_coin;
+        }
+
     }
 }
