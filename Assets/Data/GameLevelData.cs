f5b0c7f741e54324a03c6e836f46db072a9e28c9
souta_miura <miura042910@gmail.com>
2019-12-05 00:10:52 +0900

    細かいサイズ変更機能追加(。-`ω-)

--- Assets/Data/GameLevelData.cs
+++ Assets/Data/GameLevelData.cs
@@ -9,7 +9,7 @@ public class GameLevelData : ScriptableObject
     [Header("移動させる速度")] public float move_powor = 0f;
     [Header("ゲーム時間")] public float game_time_max = 0f;
     [Header("レベルアップ時に増える時間")] public float level_up_time_plus = 0f;
-    [Header("プレイヤーの初期サイズ")] public float player_scale = 1f;
+    [Header("プレイヤーのレベル毎のサイズ")] public float[] player_scale;
     [Header("カメラの初期位置（プレイヤー起点）")] public Vector3 camera_first_pos = new Vector3(0f, 0f, 0f);
     [Header("レベルアップ時にカメラを離す距離")] public Vector3 camera_moving_value = new Vector3(0f, 0f, 0f);
     [Header("１レベル時のコインの出る数")] public int coin_number = 3;
