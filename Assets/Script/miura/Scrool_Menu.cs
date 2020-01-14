fc35b9b923608ee20a2485172c65b53a53ab9399
souta_miura <miura042910@gmail.com>
2020-01-15 00:07:34 +0900

    スクロールのスクリプトをちょっと修正

--- Assets/Script/miura/Scrool_Menu.cs
+++ Assets/Script/miura/Scrool_Menu.cs
@@ -4,25 +4,30 @@ using UnityEngine;
 
 public class Scrool_Menu : MonoBehaviour
 {
-	private float first_touch_y;
-	private float now_touch_y;
-	private float difference_y;
-	private float upperlimit = 0f;
-	private float lowerlimit = 0f;
-	
-	//upperlimitはUIが移動できるy座標の最大上限,lowerlimitはUIが移動できるy座標の最大下限。
-	//最大限界値を決めておかないとUIはいくらでも移動できてしまうため
+	// 制限を掛けたいオブジェクトのrecttransform
 	[SerializeField] private RectTransform select_rect = null;
-	[SerializeField] private RectTransform canvas_select_rect = null;
+	// タッチの始点
+	private float first_touch_y = 0f;
+	// タッチの終点
+	private float now_touch_y = 0f;
+	// タッチの始点と終点の移動距離
+	private float swing_dist_y = 0f;
+	// 下にスワイプした時の移動限界
+	private float max_limit = 0f;
+	// 上にスワイプした時の移動限界
+	private float min_limit = 0f;
+	// スワイプした時の移動距離を小さくする為の変数
+	private float swing_width = 10f;
+	// スワイプ受付最大移動距離
+	private float swing_dist_max = 3f;
+	// スワイプ受付最小移動距離
+	private float swing_dist_min = -3f;
 
-    // Start is called before the first frame update
-    void Start()
+	// Start is called before the first frame update
+	void Start()
     {
-		upperlimit = select_rect.localPosition.y + 5f;
-		lowerlimit = select_rect.sizeDelta.y - canvas_select_rect.sizeDelta.y;
-		Debug.Log("上限:" + upperlimit);
-		Debug.Log("下限:" + lowerlimit);
-		Debug.Log(select_rect.localPosition.y);
+		max_limit = select_rect.localPosition.y + 5f;
+		min_limit = select_rect.sizeDelta.y - select_rect.localPosition.y;
 	}
 
 	// Update is called once per frame
@@ -33,48 +38,47 @@ public class Scrool_Menu : MonoBehaviour
 
 	void Flick()
 	{
-		//最初にInput.GetKeyDownでfirst_touch_yとしてY軸の座標を取得
+		// 最初にInput.GetKeyDownでfirst_touch_yとしてY軸の座標を取得
 		if (Input.GetKeyDown(KeyCode.Mouse0))
 		{
 			first_touch_y = Input.mousePosition.y;
 		}
 
-		//タッチしている間はnowYとしてY軸を取得して、first_touch_yとの差分difference_yを出す。
+		// タッチしている間はnowYとしてY軸を取得して、first_touch_yとの差分swing_dist_yを出す。
 		if (Input.GetKey(KeyCode.Mouse0))
 		{
 			now_touch_y = Input.mousePosition.y;
-			//nowyとfirst_touch_yの差を出す。
-			difference_y = now_touch_y - first_touch_y;
-			//スライドの振り幅を減らす。
-			difference_y /= 30f;
-			Debug.Log(select_rect.localPosition.y);
+			// nowyとfirst_touch_yの差を出す。
+			swing_dist_y = now_touch_y - first_touch_y;
+			// スライドの振り幅を減らす。
+			swing_dist_y /= swing_width;
 		}
 
-		//徐々にスクロールスピードを落としたいのでdifference_yがマイナスの時には1.5f足していく。
-		if (difference_y < -3f)
+		// 徐々にスクロールスピードを落としたいのでswing_dist_yがマイナスの時には1.5f足していく。
+		if (swing_dist_y < swing_dist_min)
 		{
-			difference_y += 1.5f;
+			swing_dist_y += 1.5f;
 		}
 
-		//徐々にスクロールスピードを落としたいのでdifference_yがプラスの時には1.5f引いていく。
-		if (difference_y > 3f)
+		// 徐々にスクロールスピードを落としたいのでswing_dist_yがプラスの時には1.5f引いていく。
+		if (swing_dist_y > swing_dist_max)
 		{
-			difference_y -= 1.5f;
+			swing_dist_y -= 1.5f;
 		}
 
-		//少しでも指をスライドしたらかなり動いてしまうので、指移動分difference_yが小さい時には移動させない。
-		if (difference_y < 3 && difference_y > -3f)
+		// 少しでも指をスライドしたらかなり動いてしまうので、指移動分swing_dist_yが小さい時には移動させない。
+		if (swing_dist_y < swing_dist_max && swing_dist_y > swing_dist_min)
 		{
-			difference_y = 0;
+			swing_dist_y = 0;
 		}
 
-		//永遠に画面がスライドできてしまうので範囲内ならスクロールさせる。
-		if (select_rect.localPosition.y > upperlimit && select_rect.localPosition.y < lowerlimit)
+		// 永遠に画面がスライドできてしまうので範囲内ならスクロールさせる。
+		if (select_rect.localPosition.y > max_limit && select_rect.localPosition.y < min_limit)
 		{
-			select_rect.localPosition += new Vector3(0, difference_y, 0);
+			select_rect.localPosition += new Vector3(0, swing_dist_y, 0);
 		}
 
-		//スワイプをして指を動かしている間、スライドを止めるためにfirst_touch_yをnow_touch_yに近づけてdifferecYを小さくする。
+		// スワイプをして指を動かしている間、スライドを止めるためにfirst_touch_yをnow_touch_yに近づけてdifferecYを小さくする。
 		if (first_touch_y < now_touch_y)
 		{
 			first_touch_y += 4f;
@@ -84,16 +88,16 @@ public class Scrool_Menu : MonoBehaviour
 			first_touch_y -= 4f;
 		}
 
-		//画面がスライドして行き過ぎた分を自動で戻す。
-		if (select_rect.localPosition.y < upperlimit)
+		// 画面がスライドして行き過ぎた分を自動で戻す。
+		if (select_rect.localPosition.y < max_limit)
 		{
-			difference_y = 0;
+			swing_dist_y = 0;
 			select_rect.localPosition += new Vector3(0, 2f, 0);
 		}
 
-		if (select_rect.localPosition.y > lowerlimit)
+		if (select_rect.localPosition.y > min_limit)
 		{
-			difference_y = 0;
+			swing_dist_y = 0;
 			select_rect.localPosition += new Vector3(0, -2f, 0);
 		}
 	}
